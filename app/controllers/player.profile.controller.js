df3a0f1b353d3e5f815b8a0b59d5564ee48096bb
Faiq Iftikhar <faiqiftikhar1@gmail.com>
2020-04-25 06:11:38 +0500

    Added Player Login, Register and Authentication. Resolves some of the sub tasks of the issue #2.

---
+++ app/controllers/player.profile.controller.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Player = require('../models/player');
+
+module.exports.profileRead = function(req, res) {
+
+  // If no user ID exists in the JWT return a 401
+  if (!req.payload._id) {
+    res.status(401).json({
+      "message" : "UnauthorizedError: private profile"
+    });
+  } else {
+    // Otherwise continue
+    Player.findById(req.payload._id).exec(function(err, user) {
+        res.status(200).json(user);
+      });
+  }
+
+};
--- app/models/player.js
+++ app/models/player.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 var game = require('./game');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
+var jwt = require("jsonwebtoken");
+var fs = require("fs");
+var crypto = require("crypto");
 
 var interests = new Schema({
     game: { type: String, required: true, lowercase: true },
@@ -15,11 +17,34 @@ var playerSchema = new Schema({
         last: { type: String, required: true, trim: true}
     },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true, minlength: 6 },
+    hash: { type: String, required: true },
+    salt: { type: String, required: true },
     opponentRanking: { type: String, enum: ['Beginner', 'Medium', 'Advance'], default: 'Beginner', required: true },
     playerRanking: { type: String, enum: ['Beginner', 'Medium', 'Advance'], default: 'Beginner', required: true },
     playerInterest: [interests]
 });
 
+playerSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+};
+
+playerSchema.methods.validatePassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    return this.hash === hash;
+};
+
+playerSchema.methods.generateToken = function() {
+    var tempDate = new Date();
+    var expiry = new Date(tempDate);
+    expiry.setMinutes(tempDate.getMinutes() + 30);      //lifespan of token -> 30 minutes...
+    
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.playerName,
+        exp: parseInt(expiry.getTime / 1000),
+    }, fs.readFileSync("F:/House/Books/Semester 8/Web Programming/Project/keys/private.key", 'utf8'));           //change this to use from environemnt variable...
+};
 
 module.exports = mongoose.model('Player', playerSchema);
