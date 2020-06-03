9500549bec7183447814695c538b49e438d0a183
Faiq Iftikhar <faiqiftikhar1@gmail.com>
2020-06-03 13:48:49 +0500

    Added mongodb connection!

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: 'mongodb+srv://spiol:Fu3k_0ff@teammeupcluster-kvtno.mongodb.net/test?retryWrites=true&w=majority',
+    uri: 'mongodb+srv://spiol:Fu3k_0ff@cluster0-kvtno.mongodb.net/test?retryWrites=true&w=majority',
     secret: crypto,
     db: 'ProjectDB'
 }
